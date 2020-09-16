1ab073c77baaabe53a24ee240b5a8a715a60182c
edrysdale <erik.drysdale@sickkids.ca>
2020-09-16 15:07:53 -0400

    no sig with Nancy

--- pipeline_GI.sh
+++ pipeline_GI.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
 # Create environment if does not exist
-source pipeline_env.sh
+#source pipeline_env.sh
 
 echo "---- CHECKING CUDA STATUS ----"
 python script_check_cuda.py
 
 # Create data
-python script_data_gen.py
+#python script_data_gen.py
 
 # Train model for each cell type
-python script_mdl_cell.py --cells eosinophil --num_epochs 500 --batch_size 2 --learning_rate 0.001 --num_params 32 --epoch_check 50
-python script_mdl_cell.py --cells eosinophil,neutrophil,plasma,lymphocyte --num_epochs 1000 --batch_size 2 --learning_rate 0.001 --num_params 32 --epoch_check 200
-
-#python script_mdl_cell.py --cells eosinophil,neutrophil,plasma,lymphocyte,other,enterocyte --num_epochs 250 --batch_size 2 --learning_rate 0.001 --num_params 16 --epoch_check 50
+#python script_mdl_cell.py --cells eosinophil --num_epochs 500 --batch_size 2 --learning_rate 0.001 --num_params 32 --epoch_check 50
+#python script_mdl_cell.py --cells eosinophil,neutrophil,plasma,lymphocyte --num_epochs 1000 --batch_size 2 --learning_rate 0.001 --num_params 32 --epoch_check 200
 
 # Create the figures to assess performance
-python script_eval.py
+#python script_eval.py
+
+# Generate the patient data
+for ii in {0..188..1}; do
+  echo $ii
+  python script_fullimg.py --ridx $ii
+done
 
 # Examine best-performing models
 python script_inference.py
+
 
 
