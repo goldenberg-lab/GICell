2bd970a53bb575d3f953e5b31b1b2a5d62b86b2b
ErikinBC <alla_hack@hotmail.com>
2021-01-04 13:43:48 -0500

    updating scripts for new images

--- pipeline_GI.sh
+++ pipeline_GI.sh
@@ -7,11 +7,11 @@ echo "---- CHECKING CUDA STATUS ----"
 python script_check_cuda.py
 
 # Create data
-#python script_data_gen.py
+python script_data_gen.py
 
 # Train model for each cell type
-#python script_mdl_cell.py --cells eosinophil --num_epochs 500 --batch_size 2 --learning_rate 0.001 --num_params 32 --epoch_check 50
-#python script_mdl_cell.py --cells eosinophil,neutrophil,plasma,lymphocyte --num_epochs 1000 --batch_size 2 --learning_rate 0.001 --num_params 32 --epoch_check 200
+python script_mdl_cell.py --cells eosinophil --num_epochs 500 --batch_size 2 --learning_rate 0.001 --num_params 32 --epoch_check 50
+python script_mdl_cell.py --cells eosinophil,neutrophil,plasma,lymphocyte --num_epochs 1000 --batch_size 2 --learning_rate 0.001 --num_params 32 --epoch_check 200
 
 # Create the figures to assess performance
 #python script_eval.py
--- script_data_gen.py
+++ script_data_gen.py
@@ -2,6 +2,7 @@
 ## --- (0) PRELIMINARIES --- ##
 
 import os
+import shutil
 import pickle
 import numpy as np
 import pandas as pd
@@ -10,9 +11,11 @@ from PIL import Image
 from funs_support import stopifnot, zip_points_parse, label_blur
 
 dir_base = os.getcwd()
-dir_images = os.path.join(dir_base, '..', 'images')
-dir_points = os.path.join(dir_base, '..', 'points')
-dir_output = os.path.join(dir_base, '..', 'output')
+dir_cell = os.path.join(dir_base, '..')
+dir_images = os.path.join(dir_cell, 'images')
+dir_points = os.path.join(dir_cell, 'points')
+dir_output = os.path.join(dir_cell, 'output')
+dir_todo = os.path.join(dir_cell, 'todo')
 
 if not os.path.exists(dir_output):
     print('output directory does not exist, creating')
@@ -21,9 +24,9 @@ if not os.path.exists(dir_output):
 valid_cells = ['eosinophil', 'neutrophil', 'plasma',
                'enterocyte', 'other', 'lymphocyte']
 
-fill = 1 # how many points to pad
-fillfac = 2*fill+1 # number of padded points (i.e. count inflator)
-s2 = 2 # gaussian blur
+fill = 1  # how many points to pad
+fillfac = 2 * fill + 1  # number of padded points (i.e. count inflator)
+s2 = 2  # gaussian blur
 
 ##################################
 ## --- (1) PREP IN THE DATA --- ##
@@ -32,22 +35,43 @@ fn_points = os.listdir(dir_points)
 fn_images = os.listdir(dir_images)
 raw_points = pd.Series(fn_points).str.split('\\.', expand=True).iloc[:, 0]
 raw_images = pd.Series(fn_images).str.split('\\.', expand=True).iloc[:, 0]
+raw_images = raw_images[raw_images.str.contains('^cleaned')].reset_index(None,True)
 qq = raw_points.isin(raw_images)
 stopifnot(qq.all())
 print('All points found in images, %i of %i images found in points' %
       (qq.sum(), len(raw_images)))
+missing_points = raw_images[~raw_images.isin(raw_points)].reset_index(None,True)
+print('There are %i images without any annotations' % (len(missing_points)))
 
-ids_tissue = pd.Series(fn_points).str.replace('.png-points.zip|cleaned\\_','')
+# TEMP: Compare to the send files
+dir_d = 'D:\\projects\GICell'
+fn_extra = pd.Series(os.listdir(os.path.join(dir_d,'extra')))
+fn_extra = fn_extra.str.split('\\.',1,True).iloc[:,0]
+fn_6th = pd.Series(os.listdir(os.path.join(dir_d,'6th')))
+fn_6th = fn_6th.str.split('\\.',1,True).iloc[:,0]
+assert fn_6th.isin(fn_extra).all()
+assert fn_6th.isin(raw_points).all()
+print('A total of %i were not included file sent\n'
+      'A total of %i were not annotated in file sent' %
+      (np.sum(~missing_points.isin(fn_extra)),
+       np.sum(np.sum(fn_extra.isin(missing_points)))))
+for fn in missing_points:
+    fn_img = fn + '.png'
+    path = os.path.join(dir_images, fn_img)
+    assert os.path.exists(path)
+    shutil.copy(path, os.path.join(dir_todo, fn_img))
+
+ids_tissue = pd.Series(fn_points).str.replace('.png-points.zip|cleaned\\_', '')
 # tmp = tmp.str.replace('\\_[0-9]{1,2}','').str.split('_',expand=True,n=1)
 # ids = tmp.iloc[:,0]
 # tissue = tmp.iloc[:,1]
 # ids_tissue = ids + '_' + tissue
 
-di_img_point = {z: {'img':[],'pts':[], 'lbls':[]} for z in ids_tissue}
+di_img_point = {z: {'img': [], 'pts': [], 'lbls': []} for z in ids_tissue}
 npoint = len(fn_points)
 for ii, fn in enumerate(fn_points):
     print('file %s (%i of %i)' % (fn, ii + 1, len(fn_points)))
-    idt = fn.replace('.png-points.zip','').replace('cleaned_','')
+    idt = fn.replace('.png-points.zip', '').replace('cleaned_', '')
     # idt = re.sub('\\_[0-9]{1,2}','', idt)
     path = os.path.join(dir_points, fn)
     df_ii = zip_points_parse(path, dir_base, valid_cells)
@@ -60,18 +84,15 @@ for ii, fn in enumerate(fn_points):
     lbls = label_blur(idx=idx_xy, cells=df_ii.cell.values, vcells=valid_cells,
                       shape=img_vals.shape[0:2], fill=fill, s2=s2)
     di_img_point[idt]['lbls'] = lbls.copy()
-    err = np.round(np.abs(np.sum(lbls) / fillfac ** 2 - idx_xy.shape[0]), 1)
-    assert err <= 1.0
+    est, true = np.sum(lbls) / fillfac ** 2, idx_xy.shape[0]
+    pct = 100*(est/true-1)
+    assert np.abs(pct) <= 2
     # from funs_support import comp_plt, val_plt
     # val_plt(arr=di_img_point[idt]['img'], pts=lbls[:,:,[0,2]],lbls=['a','b'],
     #          gt=lbls[:,:,[0,2]],path='..',thresh=[0.0,0.0],fn='blur.png')
 
 print(len(di_img_point))
-assert all([di_img_point[z]['img'].shape[0]>0 for z in di_img_point])
+assert all([di_img_point[z]['img'].shape[0] > 0 for z in di_img_point])
 
 # Save for later
-pickle.dump(di_img_point, open(os.path.join(dir_output,'di_img_point.pickle'), "wb"))
-
-
-
-
+pickle.dump(di_img_point, open(os.path.join(dir_output, 'di_img_point.pickle'), "wb"))
